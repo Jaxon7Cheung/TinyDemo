ab2a74c8c8fe34dbd7198b60ddb59309e555e9e1
Jaxon7Cheung <2104526824@qq.com>
2024-07-29 15:08:42 +0800

    RunLoop所做的事

--- 多线程编程/线程同步方案（锁）/线程同步方案（锁）/ViewController.m
+++ 多线程编程/线程同步方案（锁）/线程同步方案（锁）/ViewController.m
@@ -51,23 +51,36 @@
 //    [self.lock otherTest];
     
     
-    self.thread = [[NSThread alloc] initWithBlock:^{
-        NSLog(@"thread test");
-        
-        // 启用RunLoop
-        [[NSRunLoop currentRunLoop] addPort: [[NSPort alloc] init] forMode: NSDefaultRunLoopMode];
-        [[NSRunLoop currentRunLoop] run];
-    }];
-    [self.thread start];
-    
-    // 线程的任务一旦执行完毕，生命周期就结束，无法再使用
-    // 保持线程的生命为什么要用RunLoop？用强指针解可以吧？
-    // 答：准确来讲，使用RunLoop是为了让线程保持激活状态，用强指针只能说明thread的那块内存在那儿，但thread已然不能使用
+//    self.thread = [[NSThread alloc] initWithBlock:^{
+//        NSLog(@"thread test");
+//        
+//        // 启用RunLoop
+//        [[NSRunLoop currentRunLoop] addPort: [[NSPort alloc] init] forMode: NSDefaultRunLoopMode];
+//        [[NSRunLoop currentRunLoop] run];
+//    }];
+//    [self.thread start];
+//    
+//    // 线程的任务一旦执行完毕，生命周期就结束，无法再使用
+//    // 保持线程的生命为什么要用RunLoop？用强指针解可以吧？
+//    // 答：准确来讲，使用RunLoop是为了让线程保持激活状态，用强指针只能说明thread的那块内存在那儿，但thread已然不能使用
     
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [self performSelector: @selector(test) onThread: self.thread withObject: nil waitUntilDone: NO];
+//    [self performSelector: @selector(test) onThread: self.thread withObject: nil waitUntilDone: NO];
+    
+    
+//    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
+//    dispatch_async(queue, ^{
+//        NSLog(@"1");
+//        
+//        [self performSelector: @selector(test) withObject: nil afterDelay: .0];
+//        
+//        NSLog(@"3");
+//        [[NSRunLoop currentRunLoop] addPort: [[NSPort alloc] init] forMode: NSDefaultRunLoopMode];
+//        [[NSRunLoop currentRunLoop] run];
+//    });
+    // 主线程几乎所有的事情都是交给RunLoop去做。比如UI界面的刷新、点击事件的处理、performSelector等等
 }
 
 - (void)test {
